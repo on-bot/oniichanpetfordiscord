09f406d1b404aff9484bd8405e06b7b404755d38
oniichan-cpu <30630722+oniichan-cpu@users.noreply.github.com>
2020-10-22 16:50:31 +0545

    Update study.py

--- cogs/study.py
+++ cogs/study.py
@@ -26,14 +26,14 @@ class Study(commands.Cog):
                 logs = f.read()
             with open('test.txt', 'w') as f:
                 f.write(str(logs + "\n"))
-                f.write(strnow[:-13] + ' ' + str(t_l[0]) + ' ' + str(t_l[1]))
+                texts1 = t_l[1].split(',')
+                texts1instring = ''
+                for letter in texts1:
+                    texts1instring = texts1instring + str(letter.capitalize()) + ' '
+                f.write(strnow[:-13] + ' ' + str(t_l[0]) + ' ' + texts1instring)
 
-            await ctx.send("success")
-            await asyncio.sleep(86400/2)
-            req = requests.session()
-            files = {'file': open("test.txt", 'rb')}
-            response = req.post('https://file.io/', files=files)
-            await ctx.send(f'Uploaded {response.text}')
+            await ctx.send("Successfull")
+
         except:
             now = datetime.now(pytz.timezone('Asia/Kathmandu'))
             strnow = str(now)
@@ -42,18 +42,23 @@ class Study(commands.Cog):
             with open('test.txt', 'w') as f:
                 f.write(str(logs + "\n"))
                 f.write(strnow[:-13] + ' ' + texts.capitalize())
-            await ctx.send("success except")
-            await asyncio.sleep(86400/2)
-            req = requests.session()
-            files = {'file': open("test.txt", 'rb')}
-            response = req.post('https://file.io/', files=files)
-            await ctx.send(f'Uploaded {response.text}')
+            await ctx.send("Successful But dont use This")
+
+
+    @commands.command()
+    async def uploadlogs(self,ctx):
+        req = requests.session()
+        files = {'file': open("test.txt", 'rb')}
+        response = req.post('https://file.io/', files=files)
+        await ctx.send(f'Uploaded {response.text}')
+
 
     @commands.command()
     async def readlogs(self, ctx):
         with open('test.txt', 'r') as f:
             logs = f.read()
         await ctx.send(logs)
+
 
 def setup(client):
     client.add_cog(Study(client))
