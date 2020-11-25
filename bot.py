5c9a36b7fe1e14f17fd558b5eb4ef4ee8971282a
oniichan-cpu <30630722+oniichan-cpu@users.noreply.github.com>
2020-11-25 15:07:52 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ async def datasteal(ctx):
 @client.event
 async def on_ready():
     print("starto")
-    await client.change_presence(status=discord.Status.idle,activity=discord.Game("But You Study"))
+    await client.change_presence(status=discord.Status.idle,activity=discord.Game("Meow! Meow! Meow!"))
 
 
 
