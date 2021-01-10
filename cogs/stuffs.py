27789c8e03dd73a3104699adb240f244312ed934
oniichan-cpu <fabianmann9@gmail.com>
2021-01-10 14:40:47 +0545

    minor change

--- cogs/stuffs.py
+++ cogs/stuffs.py
@@ -33,11 +33,9 @@ class Stuffs(commands.Cog):
     @commands.command(pass_context=True)
     async def shelp(self, ctx):
         embed = discord.Embed(title="Stuffs Help", colour=discord.Colour.purple())
-        embed.add_field(name='dmafter', value='DMs You after the mentioned time(seconds)', inline=False)
-        embed.add_field(name='dm', value='DMs the mentioned user the written messages <dmstuff @user message>', inline=False)
+        embed.add_field(name='remindafter', value='DMs You after the mentioned time(seconds)', inline=False)
+        embed.add_field(name='sendtext', value='DMs the mentioned user the written messages <dmstuff @user message>', inline=False)
         embed.add_field(name='mentionafter',value='Mentions the mentioned user after specified time(seconds) <mentionafter time @user>',inline=False)
-        embed.add_field(name='h2s', value='Converts hours into seconds', inline=False)
-        embed.add_field(name='m2s', value='Converts minutes into seconds', inline=False)
         embed.add_field(name='todaydate', value='Shows Todays Date(IDK why i added this)', inline=False)
 
         await ctx.send(embed=embed)
@@ -52,26 +50,18 @@ class Stuffs(commands.Cog):
         await ctx.send(embed=embed)
 
     @commands.command()
-    async def dm(self,ctx, user: discord.User, *, message=None):
+    async def sendtext(self,ctx, user: discord.User, *, message=None):
         message = message or "This Message is sent via DM"
         await user.send(message)
 
     @commands.command()
-    async def dmafter(self,ctx, user: discord.User, timetomention):
+    async def remindafter(self,ctx, user: discord.User, timetomention):
         if timetomention.isnumeric():
             await ctx.send(f"Nyan Nyan! Goshujin-Sama I will dm you after {timetomention} seconds")
             await asyncio.sleep(int(timetomention))
             await user.send("It's time")
         else:
             await ctx.send("Input time in seconds")
-
-    @commands.command()
-    async def m2s(self,ctx,minutes):
-        if minutes.isnumeric():
-            seconds = int(minutes) * 60
-            await ctx.send(f'{minutes} minutes is {seconds} seconds')
-        else:
-            await ctx.send("Are ya dumb?")
 
     @commands.command()
     async def mentionafter(self,ctx, timetomention,user: discord.User):
@@ -81,14 +71,6 @@ class Stuffs(commands.Cog):
             await ctx.send(f"Moshi Moshi {user.mention} Soko ni imasuka?")
         else:
             await ctx.send("Input time in seconds dumbass")
-
-    @commands.command()
-    async def h2s(self,ctx,hours):
-        if hours.isnumeric():
-            seconds = int(hours) * 60 *60
-            await ctx.send(f'{hours} hours is {seconds} seconds')
-        else:
-            await ctx.send("Are ya dumb?")
             
     @commands.command()
     async def todaydate(self,ctx):
