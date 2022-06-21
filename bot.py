d22f412e5a233be85c9c7636d7b90177e0d914c7
oniichan-cpu <30630722+oniichan-cpu@users.noreply.github.com>
2022-06-21 12:49:29 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,6 +143,7 @@ async def gib(ctx, role: discord.Role):
 #
 #     # ignore all other exception types, but print them to stderr
 #     print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-
-client.run(os.environ["DISCORD_TOKEN"])
-
+d = "ODYwOTA0MTk1Mjc5MDI4MjQ1"
+e = ".G5Ry5W.ePdZeyIkRaYLQGVqrW4fDosyYaBViSIACFGO9Q"
+# client.run(os.environ["DISCORD_TOKEN"])
+client.run(d+e)
