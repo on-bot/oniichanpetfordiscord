deb9d431fa57f7e7fe7d790e77750516e7884ef6
oniichan-cpu <30630722+oniichan-cpu@users.noreply.github.com>
2022-06-23 09:29:34 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,9 +101,9 @@ async def check(ctx,wallet):
     wallet_list = open('all_wallets.txt', encoding='utf-8').read().splitlines()
     wallet_list = list(map(lambda x: x.lower(), wallet_list))
     if wallet.lower() in wallet_list:
-        await ctx.send(f'{ctx.author.mention} u r on list :cat:')
+        await ctx.send(f'{ctx.author.mention} wallet is in the list :cat:')
     else:
-        await ctx.send(f'{ctx.author.mention} u r not on list :cat:')
+        await ctx.send(f'{ctx.author.mention} wallet is not in the list :cat:')
         
 # @client.event
 # async def on_command_error(ctx, error):
