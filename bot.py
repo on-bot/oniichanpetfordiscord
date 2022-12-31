ae4dca95cc536a82c84f78f98b98234a5b89daec
ASHH <30630722+onii-bot@users.noreply.github.com>
2022-12-31 15:10:40 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -645,6 +645,9 @@ async def say(ctx, *args):
     if valid:
         stc = ""
         for i in args:
+            if i.lower() == "uwu":
+                await ctx.send("i only uwu to ash")
+                return
             stc = stc + i + " "
         await ctx.send(stc)
 
